61165917a15913c88d223143625153fd25efb017
naoki ota <on.s24jig@gmail.com>
2023-01-02 04:34:44 +0900

    setup

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    open: true,
+  },
+  plugins: [react(), tsconfigPaths()],
+});
